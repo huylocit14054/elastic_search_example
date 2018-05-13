5758359a4c810b88a123dabac6a11270460ff40d
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2018-05-13 12:09:35 +0700

    add to github

--- app/models/article.rb
+++ app/models/article.rb
@@ -17,7 +17,7 @@ class Article < ActiveRecord::Base
         query: {
           multi_match: {
             query: query,
-            fields: ['title^10', 'text']
+            fields: [:title, :text]
           }
         },
         highlight: {
@@ -33,13 +33,3 @@ class Article < ActiveRecord::Base
   end
 end
 
-# Delete the previous articles index in Elasticsearch
-Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
-
-# Create the new index with the new mapping
-Article.__elasticsearch__.client.indices.create \
-  index: Article.index_name,
-  body: { settings: Article.settings.to_hash, mappings: Article.mappings.to_hash }
-
-# Index all article records from the DB to Elasticsearch
-Article.import
